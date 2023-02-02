b6b3dc9fbcc2c21e81713dc9ce7c405bd4e57106
Vishesh Hadimani <visheshadimani@gmail.com>
2023-02-02 11:27:30 +0530

    first commit

---
+++ src/Div.js
@@ -0,0 +1,88 @@
+import "./Div.css";
+
+export default function Div() {
+    return (<div>
+        <h1>VISHESH'S PROFILE</h1>
+        <hr></hr>
+        <img src ="https://picsum.photos/300/200" alt = "Random photo cause the repo is public"></img>
+        <hr></hr>
+        <h2>About Me:</h2>
+        <p>I am an enthusiastic learner;A bit of an ambivert, if I am comfortable with someone, I will keep our friendship. I like taking up responsibilities(perfectionism). I am basically from Belgaum, Karnataka. I am really intersted in tech related stuff and I strive to be learning something new all the time. I know the basics of HTML but serious web dev is new for me and I am really excited.🤗</p>
+        <hr></hr>
+        <h2>My Qualifications:</h2>
+        <div>
+        <ol>
+            <li>KLES' International School, Belgaum - 90.2%.</li>
+            <li>KLES' Intdependent PU College, Belgaum - 94.8%.</li>
+            <li>Sahyadri college of Engineering and Manaagement, Mangalore - CGPA: 9.35.</li>
+        </ol>
+        </div>
+        <hr></hr>
+        <h2>My hobbies:</h2>
+        <div>
+        <ul>
+            <li>Social Media Consumption- Reading articles about technology, G.K., interesting facts, etc.</li>
+            <li>Watching anime: Slice of life, Romcom, Shonen, Fantasy</li>
+            <li>Food: Going to new places and trying out new dishes.</li>
+            <li>Sleeping: A well-rested body can do wonders.</li>
+        </ul></div>
+        <hr></hr>
+        <h2>My Skills:</h2><div>
+        <ul>
+            <li>Leadership</li>
+        <li>Empathy</li>
+        <li>Analytics</li>
+        <li>Communications</li>
+        <li>Coding</li>
+        </ul></div>
+        <hr></hr>
+        <h2>My Achievements:</h2><div>
+        <ol>
+            <li>Survived Covid</li>
+            <li>Leadership roles on various occasions.</li>
+        </ol></div>
+        <hr></hr>
+        <h2>My Strengths:</h2><div>
+        <ol>
+            <li>Empathy</li>
+            <li>Leadership</li>
+            <li>Curosity</li>
+        </ol></div>
+        <hr></hr>
+        <h2>My Weaknesses:</h2><div>
+        <ol>
+            <li>Procrastination</li>
+            <li>Laziness</li>
+            <li>No seriousness</li>
+        </ol></div>
+        <hr></hr>
+        <h2>Favourite food:</h2>
+        <table border="1">
+            <tr>
+                <th>Sl. No.</th>
+                <th>Vegetarian</th>
+                <th>Non-vegetarian</th>
+                <th>Desserts</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Paneer Butter Masala</td>
+                <td>Shawarma</td>
+                <td>Falooda</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Aloo Parantha</td>
+                <td>Tandoori Chicken</td>
+                <td>Cassata</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>French fries</td>
+                <td>Biryani</td>
+                <td>Tiramisu</td>
+            </tr>
+        </table>
+        </div>
+    );
+}
